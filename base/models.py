b633f0dfd0ce0cbf9230cf42994b92d5b34dac5e
BrivaHamisi <brivahamisi@gmail.com>
2024-09-19 16:56:12 +0300

    UPDATE: StudBuddy Stable

--- base/models.py
+++ base/models.py
@@ -8,17 +8,18 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
+
 class Room(models.Model):
-    host=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    # participants = models.ManyToManyField(User)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-updated','-created']
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
@@ -30,6 +31,10 @@ class Message(models.Model):
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
 
     def __str__(self):
         return self.body[0:50]
